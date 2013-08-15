c835d6b8bd5bf24ed156ba915d4617fda6c94c8a
Xavier Fornes <xfornesa@gmail.com>
2013-08-15 16:49:38 +0200

    install and configure MopaBootstrapBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
             new Prunatic\WebBundle\PrunaticWebBundle(),
         );
 
