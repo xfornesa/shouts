323adced3b446b96bfcb798284f9e939d1cd0678
Xavier Fornes <xfornesa@gmail.com>
2013-09-15 20:05:22 +0200

    changed urls for ranks (by country, province, city)

--- src/Prunatic/WebBundle/Controller/RankController.php
+++ src/Prunatic/WebBundle/Controller/RankController.php
@@ -11,16 +11,34 @@ class RankController extends Controller
 {
     public function showByCityAction($country, $province, $city)
     {
+        $em = $this->getDoctrine()->getManager()->getRepository('PrunaticWebBundle:City');
+        $city = $em->findBySlugWithProvinceAndCountry($city, $province, $country);
+        if (!$city) {
+            $this->createNotFoundException();
+        }
+
         return new \Symfony\Component\HttpFoundation\Response('<html><body>TO DO</body></html>');
     }
 
     public function showByProvinceAction($country, $province)
     {
+        $em = $this->getDoctrine()->getManager()->getRepository('PrunaticWebBundle:Province');
+        $province = $em->findBySlugWithCountry($province, $country);
+        if (!$province) {
+            $this->createNotFoundException();
+        }
+
         return new \Symfony\Component\HttpFoundation\Response('<html><body>TO DO</body></html>');
     }
 
     public function showByCountryAction($country)
     {
+        $em = $this->getDoctrine()->getManager()->getRepository('PrunaticWebBundle:Country');
+        $country = $em->findBySlug($country);
+        if (!$country) {
+            $this->createNotFoundException();
+        }
+
         return new \Symfony\Component\HttpFoundation\Response('<html><body>TO DO</body></html>');
     }
 }
